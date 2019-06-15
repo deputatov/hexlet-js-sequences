15c2c23163ea672c756ba2fa661e01b78bc23fbe
Deputatov Nikita <deputatovn@yandex.ru>
2019-06-15 18:37:51 +0300

    corrected list.js

--- list/list.js
+++ list/list.js
@@ -1,25 +1,18 @@
-const has = (list, element) => {
+export const has = (list, element) => {
     if (isEmpty(list)) {
         return false
     }
     return head(list) === element ? true : has(tail(list), element)
 }
 
-const reverse = (list) => {
+export const reverse = (list) => {
     const iter = (l, rl) => isEmpty(l) ? rl : iter(tail(l), cons(head(l), rl))
     return iter(list, l())
 }
 
-const concat = (list1, list2) => {
+export const concat = (list1, list2) => {
     if (isEmpty(list1)) {
         return list2
     }
-    return concat(tail(list1), cons(tail(list1), list2))
-}
-
-const concat = (list1, list2) => {
-    if (isEmpty(list2)) {
-        return list1
-    }
-    return concat(cons(list1, l(head(list2))), tail(list2))
+    return cons(head(list1), concat(tail(list1), list2))
 }
