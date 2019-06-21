0e8c78ad34e4f0de1e842f24d139579caa50ba31
Deputatov Nikita <deputatovn@yandex.ru>
2019-06-21 17:20:32 +0300

    add calculatePolygonPerimeter.js

--- js_sequences_polygon_perimeter/calculatePolygonPerimeter.js
+++ js_sequences_polygon_perimeter/calculatePolygonPerimeter.js
@@ -1,6 +1,16 @@
+const calculateDistance = (p1, p2) => {
+    return Math.sqrt((getX(p2) - getX(p1)) ** 2 + (getY(p2) - getY(p1)) ** 2)
+}
 
-export const calculatePolygonPerimeter = (points) => {
-    const iter = ()
-
-
+export default (points) => {
+    if (isEmpty(points)) {
+        return null
+    }
+    const iter = (items, acc, counter) => {
+        if (isEmpty(tail(items))) {
+            return counter + 1 < 3 ? null : acc + calculateDistance(head(points), head(items))
+        }
+        return iter(tail(items), acc + calculateDistance(head(items), head(tail(items))), counter + 1)
+    }
+    return iter(points, 0, 0)
 }
