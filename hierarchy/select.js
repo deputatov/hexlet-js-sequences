c4a51981ddb6edd5409043e0e482ebe1e9c0591b
Deputatov Nikita <deputatovn@yandex.ru>
2019-06-22 15:51:06 +0300

    added teacher's solution

--- hierarchy/select.js
+++ hierarchy/select.js
@@ -11,3 +11,10 @@ const select = (tagName, elements) => {
     return reduce(predicate, l(), elements)
 }
 export default select;
+
+const select = (tagName, html) => reduce((element, acc) => {
+    const acc2 = hasChildren(element) ? concat(select(tagName, children(element)), acc) : acc;
+    return is(tagName, element) ? consList(element, acc2) : acc2;
+}, l(), html);
+
+export default select;
