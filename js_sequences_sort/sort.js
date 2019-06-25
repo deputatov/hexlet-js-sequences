efa9fbb0d23cece9a0296d3ee091b0f58a1a67e7
Deputatov Nikita <deputatovn@yandex.ru>
2019-06-25 14:43:00 +0300

    added teacher solution

--- js_sequences_sort/sort.js
+++ js_sequences_sort/sort.js
@@ -8,3 +8,19 @@ const sort = (elements) => {
     return concat(sort(lList), cons(pivot, sort(rList)))
 }
 export default sort
+
+const sort = (list) => {
+    if (isEmpty(list)) {
+        return l();
+    }
+
+    const divisor = head(list);
+    const rest = tail(list);
+
+    const left = filter(value => value <= divisor, rest);
+    const right = filter(value => value > divisor, rest);
+
+    return concat(sort(left), cons(divisor, sort(right)));
+};
+
+export default sort;
